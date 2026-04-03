dd2b5e69ad65f56f2fe4557c9210d94946cdc82a
Rene <dwaddle@users.noreply.github.com>
2026-04-03 16:23:07 +0200

    Add borders and color support per viewport to screen module

--- src/screen.asm
+++ src/screen.asm
@@ -1,4 +1,4 @@
-; screen.asm - Viewport-based Screen Control (x86_64 Linux)
+; screen.asm - Viewport-based Screen Control with Colors and Borders
 %include "algemeen.mac"
 %include "syscalls.inc"
 %include "screen.mac"
@@ -8,18 +8,25 @@ section .data
     esc_reset       db 27, '[0m', 0
     esc_hide        db 27, '[?25l', 0
     esc_show        db 27, '[?25h', 0
+    esc_fg_pre      db 27, '[3', 0
     esc_bg_pre      db 27, '[4', 0
     esc_scroll_pre  db 27, '[', 0
+    
+    ; Border characters (ASCII)
+    b_tl db '+', 0
+    b_tr db '+', 0
+    b_bl db '+', 0
+    b_br db '+', 0
+    b_h  db '-', 0
+    b_v  db '|', 0
 
 section .text
     global _screen_clear, _screen_reset_color, _screen_set_bgcolor, _screen_hide_cursor, _screen_show_cursor
-    global _viewport_create, _viewport_write, _viewport_render, _viewport_scroll, _viewport_clear
+    global _viewport_create, _viewport_write, _viewport_render, _viewport_scroll, _viewport_set_color, _viewport_set_border
     
     extern PrintString, _int_to_str, _mem_alloc, _mem_set, _mem_copy, _cursor_goto_xy, _strlen
 
 ; --- [ Viewport Aanmaken ] ---
-; Input: RDI=X, RSI=Y, RDX=W, RCX=H, R8=BufH
-; Output: RAX = Pointer naar Viewport Struct
 _viewport_create:
     @save_context
     mov r12, rdi ; X
@@ -28,29 +35,27 @@ _viewport_create:
     mov r15, rcx ; H
     mov rbp, r8  ; BufH
 
-    ; 1. Alloceren van de structuur (56 bytes)
     mov rdi, VP_SIZE
     call _mem_alloc
-    push rax     ; Bewaar struct pointer
+    push rax
 
-    ; 2. Vul de structuur
     mov [rax + VP_X], r12
     mov [rax + VP_Y], r13
     mov [rax + VP_W], r14
     mov [rax + VP_H], r15
     mov [rax + VP_BUF_H], rbp
     mov qword [rax + VP_VIEW_Y], 0
+    mov byte [rax + VP_FG], 7 ; White
+    mov byte [rax + VP_BG], 0 ; Black
+    mov byte [rax + VP_FLAGS], 0
 
-    ; 3. Alloceren van de tekstbuffer (BufH * W)
     mov rax, rbp
     mul r14
     mov rdi, rax
     call _mem_alloc
-    
-    pop rbx      ; Haal struct pointer terug
+    pop rbx
     mov [rbx + VP_BUF], rax
     
-    ; 4. Buffer leegmaken (spaties)
     mov rdi, rax
     mov rsi, ' '
     mov rax, [rbx + VP_BUF_H]
@@ -58,12 +63,29 @@ _viewport_create:
     mov rdx, rax
     call _mem_set
 
-    mov rax, rbx ; Return struct pointer
+    mov rax, rbx
     @restore_context
     ret
 
+; --- [ Viewport Kleur Instellen ] ---
+; Input: RDI=VP, RSI=FG, RDX=BG
+_viewport_set_color:
+    mov [rdi + VP_FG], sil
+    mov [rdi + VP_BG], dl
+    ret
+
+; --- [ Viewport Border Instellen ] ---
+; Input: RDI=VP, RSI=Status (1/0)
+_viewport_set_border:
+    test rsi, rsi
+    jz .disable
+    or byte [rdi + VP_FLAGS], VPF_BORDER
+    ret
+.disable:
+    and byte [rdi + VP_FLAGS], ~VPF_BORDER
+    ret
+
 ; --- [ Schrijf naar Viewport ] ---
-; Input: RDI=VP_Ptr, RSI=LocalX, RDX=LocalY, RCX=String_Ptr
 _viewport_write:
     @save_context
     mov r12, rdi ; VP
@@ -71,12 +93,10 @@ _viewport_write:
     mov r14, rdx ; LY
     mov r15, rcx ; Str
 
-    ; 1. Bereken lengte en check bounds
     mov rdi, r15
     call _strlen
-    mov rbx, rax ; Lengte
+    mov rbx, rax
 
-    ; Truncatie check breedte
     mov rax, r13
     add rax, rbx
     cmp rax, [r12 + VP_W]
@@ -85,50 +105,58 @@ _viewport_write:
     sub rax, r13
     mov rbx, rax
 .do_copy:
-    ; 2. Bereken offset in buffer: (LY * W) + LX
     mov rax, r14
     mul qword [r12 + VP_W]
     add rax, r13
     add rax, [r12 + VP_BUF]
-    
-    mov rdi, rax ; Dest
-    mov rsi, r15 ; Src
-    mov rdx, rbx ; Count
+    mov rdi, rax
+    mov rsi, r15
+    mov rdx, rbx
     call _mem_copy
     @restore_context
     ret
 
 ; --- [ Render Viewport ] ---
-; Input: RDI = VP_Ptr
 _viewport_render:
     @save_context
     mov r12, rdi ; VP
-    
     call _screen_hide_cursor
-    
-    mov r13, 0   ; r13 = huidige zichtbare regel (0 tot VP_H-1)
+
+    ; 1. Optioneel: Border Tekenen
+    test byte [r12 + VP_FLAGS], VPF_BORDER
+    jz .skip_border
+    call _viewport_draw_border_internal
+.skip_border:
+
+    ; 2. Kleuren instellen
+    call _viewport_apply_color_internal
+
+    ; 3. Inhoud renderen
+    mov r13, 0 ; Visible Line
 .line_loop:
-    ; Bereken buffer regel index: VP_VIEW_Y + r13
     mov rax, [r12 + VP_VIEW_Y]
     add rax, r13
     cmp rax, [r12 + VP_BUF_H]
     jae .done_rendering
     
-    ; Bereken start van regel in buffer
     mul qword [r12 + VP_W]
     add rax, [r12 + VP_BUF]
-    mov r14, rax ; r14 = buffer regel pointer
-    
-    ; Zet cursor op fysieke schermpositie
-    ; Rij = VP_Y + r13 + 1, Kolom = VP_X + 1
+    mov r14, rax
+
+    ; Fysieke positie (houd rekening met border indien aanwezig)
     mov rdi, [r12 + VP_Y]
     add rdi, r13
     inc rdi
     mov rsi, [r12 + VP_X]
     inc rsi
+    
+    test byte [r12 + VP_FLAGS], VPF_BORDER
+    jz .no_border_offset
+    inc rdi
+    inc rsi
+.no_border_offset:
     call _cursor_goto_xy
     
-    ; Schrijf de regel
     mov rax, SYS_WRITE
     mov rdi, 1
     mov rsi, r14
@@ -140,21 +168,125 @@ _viewport_render:
     jb .line_loop
 
 .done_rendering:
+    call _screen_reset_color
     call _screen_show_cursor
     @restore_context
     ret
 
+; --- [ Interne Helpers ] ---
+
+_viewport_apply_color_internal:
+    ; Voorgrond
+    mov rdi, esc_fg_pre
+    call PrintString
+    movzx rax, byte [r12 + VP_FG]
+    sub rsp, 16
+    add al, '0'
+    mov [rsp], al
+    mov byte [rsp+1], 'm'
+    mov byte [rsp+2], 0
+    mov rdi, rsp
+    call PrintString
+    add rsp, 16
+    
+    ; Achtergrond
+    mov rdi, esc_bg_pre
+    call PrintString
+    movzx rax, byte [r12 + VP_BG]
+    sub rsp, 16
+    add al, '0'
+    mov [rsp], al
+    mov byte [rsp+1], 'm'
+    mov byte [rsp+2], 0
+    mov rdi, rsp
+    call PrintString
+    add rsp, 16
+    ret
+
+_viewport_draw_border_internal:
+    ; Teken een box rond de viewport (X, Y tot X+W+1, Y+H+1)
+    mov r14, [r12 + VP_W]
+    add r14, 1 ; Rechterkant
+    mov r15, [r12 + VP_H]
+    add r15, 1 ; Onderkant
+
+    ; TL Corner
+    mov rdi, [r12 + VP_Y]
+    inc rdi
+    mov rsi, [r12 + VP_X]
+    inc rsi
+    call _cursor_goto_xy
+    mov rdi, b_tl
+    call PrintString
+
+    ; Top edge
+    mov rcx, [r12 + VP_W]
+.t_loop:
+    mov rdi, b_h
+    push rcx
+    call PrintString
+    pop rcx
+    loop .t_loop
+
+    ; TR Corner
+    mov rdi, b_tr
+    call PrintString
+
+    ; Vertical lines
+    mov rcx, [r12 + VP_H]
+    mov r13, 1
+.v_loop:
+    push rcx
+    mov rdi, [r12 + VP_Y]
+    add rdi, r13
+    inc rdi
+    mov rsi, [r12 + VP_X]
+    inc rsi
+    call _cursor_goto_xy
+    mov rdi, b_v
+    call PrintString
+    
+    mov rsi, [r12 + VP_X]
+    add rsi, [r12 + VP_W]
+    add rsi, 2
+    call _cursor_goto_xy
+    mov rdi, b_v
+    call PrintString
+    
+    inc r13
+    pop rcx
+    loop .v_loop
+
+    ; Bottom line
+    mov rdi, [r12 + VP_Y]
+    add rdi, [r12 + VP_H]
+    add rdi, 2
+    mov rsi, [r12 + VP_X]
+    inc rsi
+    call _cursor_goto_xy
+    mov rdi, b_bl
+    call PrintString
+    
+    mov rcx, [r12 + VP_W]
+.b_loop:
+    mov rdi, b_h
+    push rcx
+    call PrintString
+    pop rcx
+    loop .b_loop
+    
+    mov rdi, b_br
+    call PrintString
+    ret
+
 ; --- [ Scroll Viewport ] ---
-; Input: RDI = VP_Ptr, RSI = Aantal (+/-)
 _viewport_scroll:
     @save_context
     mov r12, rdi
     mov rax, [r12 + VP_VIEW_Y]
     add rax, rsi
-    
     test rax, rax
     js .set_zero
-    
     mov rbx, [r12 + VP_BUF_H]
     sub rbx, [r12 + VP_H]
     cmp rax, rbx
@@ -170,7 +302,7 @@ _viewport_scroll:
     @restore_context
     ret
 
-; --- [ Algemene Functies ] ---
+; --- [ Basis Functies ] ---
 _screen_clear:
     mov rdi, esc_clear
     call PrintString
--- tests/test_viewports.asm
+++ tests/test_viewports.asm
@@ -1,9 +1,9 @@
-; test_viewports.asm - Test for Multiple Independent Viewports
+; test_viewports.asm - Test for Multiple Viewports with Colors and Borders
 %include "algemeen.mac"
 %include "screen.mac"
 
 section .data
-    msg_start  db "Viewport Demo: Sidebar (links) en Main (rechts)", 0
+    msg_start   db "Viewport Demo: Sidebar (rand) en Main (kleur)", 0
     sidebar_txt db "SIDEBAR", 0
     main_txt    db "MAIN CONTENT LINE #", 0
     press_key   db "Druk op een toets om RECHTS te scrollen...", 0
@@ -15,26 +15,30 @@ section .bss
 
 section .text
     global _start
-    extern _viewport_create, _viewport_write, _viewport_render, _viewport_scroll
+    extern _viewport_create, _viewport_write, _viewport_render, _viewport_scroll, _viewport_set_color, _viewport_set_border
     extern _screen_clear, PrintString, ReadChar, _int_to_str, _cursor_goto_xy
 
 _start:
     call _screen_clear
     
-    ; 1. Maak Sidebar (X=0, Y=0, W=20, H=20, BufH=20)
-    @vp_create 0, 0, 20, 20, 20
+    ; 1. Maak Sidebar (X=0, Y=0, W=15, H=15, BufH=15)
+    @vp_create 0, 0, 15, 15, 15
     mov [vp_sidebar], rax
+    @vp_set_border [vp_sidebar], 1
+    @vp_set_color  [vp_sidebar], 3, 0 ; Yellow on Black
     
-    ; 2. Maak Main Window (X=22, Y=0, W=55, H=20, BufH=100)
-    @vp_create 22, 0, 55, 20, 100
+    ; 2. Maak Main Window (X=20, Y=0, W=55, H=15, BufH=100)
+    @vp_create 20, 0, 55, 15, 100
     mov [vp_main], rax
+    @vp_set_border [vp_main], 1
+    @vp_set_color  [vp_main], 2, 4 ; Green on Blue
     
     ; 3. Vul Sidebar
     mov r12, 0
 .fill_sidebar:
     @vp_write [vp_sidebar], 2, r12, sidebar_txt
     inc r12
-    cmp r12, 20
+    cmp r12, 15
     jb .fill_sidebar
     
     ; 4. Vul Main Window met 100 regels
@@ -56,7 +60,7 @@ _start:
     @vp_render [vp_main]
     
     ; 6. Interactie
-    mov rdi, 22
+    mov rdi, 20
     mov rsi, 1
     call _cursor_goto_xy
     mov rdi, press_key
@@ -64,18 +68,13 @@ _start:
     
     call ReadChar
     
-    ; Scroll alleen het rechter scherm 10 regels omlaag
+    ; Scroll Main Window
     @vp_scroll [vp_main], 10
     
     call ReadChar
     
-    ; Scroll nog 20 regels
+    ; Scroll nog eens
     @vp_scroll [vp_main], 20
-    
-    call ReadChar
-    
-    ; Scroll Sidebar (die maar 20 regels heeft, dus scrollen doet niks)
-    @vp_scroll [vp_sidebar], 5
     
     call ReadChar
     
